876c54d7964a936ff8cdd473ea88f39a00e7733a
Mohamad Hussein <mhamad1assaf@gmail.com>
2022-06-13 15:22:55 +0300

    Presentational and Container Components

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -7,19 +7,9 @@ class DishDetail extends Component {
 
   }
   renderDish(dish) {
-    if(dish != null){
-        const comment = dish.comments.map((comment) => {
-          return(
-            <ul class="list-unstyled">
-              <li>{comment.comment}</li>
-              <li>--{comment.author} , {comment.date}</li>
-            </ul>
-          )
-        })
-      
+    if(dish != null){   
       return(
-        <div className="row">
-        <div className="col-12 col-md-5 m-1">
+        <div>
           <Card>
           <CardImg width='100%' src={dish.image} alt={dish.name} />
           <CardBody>
@@ -27,22 +17,46 @@ class DishDetail extends Component {
             <CardText>{dish.description}</CardText>
           </CardBody>
         </Card>
-      </div>
-      <div className="col-12 col-md-5 m-1">
-        <h4>Comments</h4>
-        {comment}
-      </div>
-      </div>
+      </div>        
       )} else {
         return(<div></div>)
       }
   }
+  renderComment(comments) {
+    if(comments != null) {
+      console.log('comment')
+      const comment = comments.map((comment) => {
+        return(
+          <ul class="list-unstyled">
+            <li>{comment.comment}</li>
+            <li>--{comment.author} , {new Intl.DateTimeFormat('en-US', 
+            {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+          </ul>
+        )
+      })
+      return(
+        <div>
+          <h4>Comments</h4>
+          {comment}
+        </div>
+      )
+    } else {
+      console.log('else')
+      return(<div></div>)
+    }
+  }
   render() {
-      
     return(
-      <>
-      {this.renderDish(this.props.dish)}
-      </>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-12 col-md-5 m-1">        
+            {this.renderComment(this.props.dish?.comments)}
+          </div>
+        </div>
+      </div>
     )
   }
 }
