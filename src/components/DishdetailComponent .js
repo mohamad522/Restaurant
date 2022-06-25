f8d8b8dde88f5fefdae2f1f0a4b2381340440abb
Mohamad Hussein <mhamad1assaf@gmail.com>
2022-06-25 11:54:56 +0300

    Redux Thunk

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -17,6 +17,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -35,8 +36,13 @@ class CommentForm extends Component {
 		});
 	}
 	handleSubmit(values) {
-    this.toggleCommentModal();
-		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+		this.toggleCommentModal();
+		this.props.addComment(
+			this.props.dishId,
+			values.rating,
+			values.author,
+			values.comment
+		);
 		console.log("Current State is: " + JSON.stringify(values));
 		alert("Current State is: " + JSON.stringify(values));
 	}
@@ -133,81 +139,92 @@ class CommentForm extends Component {
 }
 
 function RenderDish({ dish }) {
-	if (dish != null) {
-		return (
-			<div>
-				<Card>
-					<CardImg width="100%" src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
-			</div>
-		);
-	} else {
-		return <div></div>;
-	}
-}
-function RenderComments({comments, addComment, dishId}) {
-	if (comments != null) {
-		console.log("comment");
-		const comment = comments.map((comment) => {
-			return (
-				<ul class="list-unstyled">
-					<li>{comment.comment}</li>
-					<li>
-						--{comment.author} ,{" "}
-						{new Intl.DateTimeFormat("en-US", {
-							year: "numeric",
-							month: "short",
-							day: "2-digit",
-						}).format(new Date(Date.parse(comment.date)))}
-					</li>
-				</ul>
-			);
-		});
-		return (
-			<div>
-				<h4>Comments</h4>
-				{comment}
-				<CommentForm dishId={dishId} addComment={addComment} />
-			</div>
-		);
-	} else {
-		console.log("else");
-		return <div></div>;
-	}
-}
-const DishDetail = (props) => {
 	return (
-		<div className="container">
-			<div className="row">
-				<Breadcrumb>
-					<BreadcrumbItem>
-						<Link to="/menu">Menu</Link>
-					</BreadcrumbItem>
-					<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-				</Breadcrumb>
-				<div className="col-12">
-					<h3>{props.dish.name}</h3>
-					<hr />
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-12 col-md-5 m-1">
-					<RenderDish dish={props.dish} />
-				</div>
-				<div className="col-12 col-md-5 m-1">
-					<RenderComments
-						comments={props.comments}
-						addComment={props.addComment}
-						dishId={props.dish.id}
-					/>
-				</div>
-			</div>
+		<div>
+			<Card>
+				<CardImg width="100%" src={dish.image} alt={dish.name} />
+				<CardBody>
+					<CardTitle>{dish.name}</CardTitle>
+					<CardText>{dish.description}</CardText>
+				</CardBody>
+			</Card>
 		</div>
 	);
+}
+function RenderComments({ comments, addComment, dishId }) {
+	console.log("comment");
+	const comment = comments.map((comment) => {
+		return (
+			<ul class="list-unstyled">
+				<li>{comment.comment}</li>
+				<li>
+					--{comment.author} ,{" "}
+					{new Intl.DateTimeFormat("en-US", {
+						year: "numeric",
+						month: "short",
+						day: "2-digit",
+					}).format(new Date(Date.parse(comment.date)))}
+				</li>
+			</ul>
+		);
+	});
+	return (
+		<div>
+			<h4>Comments</h4>
+			{comment}
+			<CommentForm dishId={dishId} addComment={addComment} />
+		</div>
+	);
+}
+const DishDetail = (props) => {
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	} else if (props.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	} else if (props.dish != null) {
+			return (
+				<div className="container">
+					<div className="row">
+						<Breadcrumb>
+							<BreadcrumbItem>
+								<Link to="/menu">Menu</Link>
+							</BreadcrumbItem>
+							<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+						</Breadcrumb>
+						<div className="col-12">
+							<h3>{props.dish.name}</h3>
+							<hr />
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-12 col-md-5 m-1">
+							<RenderDish dish={props.dish} />
+						</div>
+						<div className="col-12 col-md-5 m-1">
+							<RenderComments
+								comments={props.comments}
+								addComment={props.addComment}
+								dishId={props.dish.id}
+							/>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return <div></div>;
+		}
 };
 
 export default DishDetail;
