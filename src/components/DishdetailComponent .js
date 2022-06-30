076c9638adb647e1ac99bbbd5a717900d2352af1
Mohamad Hussein <mhamad1assaf@gmail.com>
2022-06-30 10:17:24 +0300

    React Animation Components

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -18,7 +18,8 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
-import { baseUrl } from '../shared/baseUrl';
+import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
@@ -142,13 +143,20 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
 	return (
 		<div>
-			<Card>
-				<CardImg top src={baseUrl + dish.image} alt={dish.name} />
-				<CardBody>
-					<CardTitle>{dish.name}</CardTitle>
-					<CardText>{dish.description}</CardText>
-				</CardBody>
-			</Card>
+			<FadeTransform
+				in
+				transformProps={{
+					exitTransform: "scale(0.5) translateY(-50%)",
+				}}
+			>
+				<Card>
+					<CardImg top src={baseUrl + dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+			</FadeTransform>
 		</div>
 	);
 }
@@ -157,15 +165,25 @@ function RenderComments({ comments, postComment, dishId }) {
 	const comment = comments.map((comment) => {
 		return (
 			<ul class="list-unstyled">
-				<li>{comment.comment}</li>
-				<li>
-					--{comment.author} ,{" "}
-					{new Intl.DateTimeFormat("en-US", {
-						year: "numeric",
-						month: "short",
-						day: "2-digit",
-					}).format(new Date(Date.parse(comment.date)))}
-				</li>
+				<Stagger in>
+					{comments.map((comment) => {
+						return (
+							<Fade in>
+								<li key={comment.id}>
+									<p>{comment.comment}</p>
+									<p>
+										-- {comment.author} ,{" "}
+										{new Intl.DateTimeFormat("en-US", {
+											year: "numeric",
+											month: "short",
+											day: "2-digit",
+										}).format(new Date(Date.parse(comment.date)))}
+									</p>
+								</li>
+							</Fade>
+						);
+					})}
+				</Stagger>
 			</ul>
 		);
 	});
@@ -195,37 +213,37 @@ const DishDetail = (props) => {
 			</div>
 		);
 	} else if (props.dish != null) {
-			return (
-				<div className="container">
-					<div className="row">
-						<Breadcrumb>
-							<BreadcrumbItem>
-								<Link to="/menu">Menu</Link>
-							</BreadcrumbItem>
-							<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-						</Breadcrumb>
-						<div className="col-12">
-							<h3>{props.dish.name}</h3>
-							<hr />
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12 col-md-5 m-1">
-							<RenderDish dish={props.dish} />
-						</div>
-						<div className="col-12 col-md-5 m-1">
-							<RenderComments
-								comments={props.comments}
-								postComment={props.postComment}
-								dishId={props.dish.id}
-							/>
-						</div>
+		return (
+			<div className="container">
+				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem>
+							<Link to="/menu">Menu</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{props.dish.name}</h3>
+						<hr />
 					</div>
 				</div>
-			);
-		} else {
-			return <div></div>;
-		}
+				<div className="row">
+					<div className="col-12 col-md-5 m-1">
+						<RenderDish dish={props.dish} />
+					</div>
+					<div className="col-12 col-md-5 m-1">
+						<RenderComments
+							comments={props.comments}
+							postComment={props.postComment}
+							dishId={props.dish.id}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return <div></div>;
+	}
 };
 
 export default DishDetail;
